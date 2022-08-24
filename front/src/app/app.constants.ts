920180ad1a7925aa5d71cc8f4b5bb559a7d26e3a
ivanaGvero <81170381+ivanaGvero@users.noreply.github.com>
2022-08-24 15:01:56 +0200

    izmena

--- front/src/app/app.constants.ts
+++ front/src/app/app.constants.ts
@@ -1,5 +1,11 @@
-export const FAKULTET_URL= 'http://localhost:8083/fakultet';
-export const STATUS_URL= 'http://localhost:8083/status';
-export const DEPARTMAN_URL= 'http://localhost:8083/departman';
-export const STUDENT_URL= 'http://localhost:8083/student';
-export const STUDENT_DEP_URL = 'http://localhost:8083/studentSaDepartmana';
+//export const FAKULTET_URL= 'http://localhost:8083/fakultet';
+//export const STATUS_URL= 'http://localhost:8083/status';
+//export const DEPARTMAN_URL= 'http://localhost:8083/departman';
+//export const STUDENT_URL= 'http://localhost:8083/student';
+//export const STUDENT_DEP_URL = 'http://localhost:8083/studentSaDepartmana';
+
+export const FAKULTET_URL= 'https://gvero.herokuapp.com/fakultet';
+export const STATUS_URL= 'https://gvero.herokuapp.com/status';
+export const DEPARTMAN_URL= 'https://gvero.herokuapp.com/departman';
+export const STUDENT_URL= 'https://gvero.herokuapp.com/student';
+export const STUDENT_DEP_URL = 'https://gvero.herokuapp.com/studentSaDepartmana';
